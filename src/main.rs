a65a68764d041f089553d6c50c14a94f78d5ad9f
drewlong314 <drewlong314@gmail.com>
2023-02-17 01:19:38 -0600

    add temperature unit flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env::args;
+
 use serde::Deserialize;
 #[tokio::main]
 
@@ -9,20 +11,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     #[derive(Deserialize)]
-    struct Orange {
+    struct Response {
         current_weather: CurrentWeather,
     }
 
-    let resp =
-        reqwest::get("https://api.open-meteo.com/v1/forecast?latitude=33.52&longitude=-86.80&current_weather=true&temperature_unit=fahrenheit")
-            .await?
-            .json::<Orange>()
-            .await?;
+    // TODO: Add option to set location and Temperature Unit in an env
+    // TODO: Show full day's forecast
+    // TODO: Show full week's Highs and Low's
+    // TODO: Create a lib.rs so that main.rs is more readable
+
+    let mut temperature_unit = "C";
+    let mut url = String::from("https://api.open-meteo.com/v1/forecast?latitude=33.52&longitude=-86.80&current_weather=true");
+    let args = args();
+    if args.len() > 0 {
+        for arg in args {
+            match arg.as_str() {
+                "F" => {
+                    url = url + "&temperature_unit=fahrenheit";
+                    temperature_unit = "F";
+                }
+                _ => (),
+            }
+        }
+    }
+
+    let resp = reqwest::get(url).await?.json::<Response>().await?;
 
     let current_weather = resp.current_weather;
     println!(
-        "Current Temperature: {:#?}°F",
-        current_weather.temperature as i32
+        "Current Temperature: {:#?}°{}",
+        current_weather.temperature as i32, temperature_unit
     );
     println!(
         "Current Weather Condition: {}",
@@ -32,6 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+// TODO: Add the rest of the weather conditions
 fn convert_to_weather_condition(code: i32) -> String {
     match code {
         51 => String::from("Light Drizzle"),
