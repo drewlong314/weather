3f2ab4dfef2376aa97b91de9f8b74b965336b42d
drewlong314 <drewlong314@gmail.com>
2023-02-17 00:40:22 -0600

    get current weather

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,39 @@ use serde::Deserialize;
 #[tokio::main]
 
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[derive(Deserialize, Debug)]
+    struct CurrentWeather {
+        temperature: f32,
+        weathercode: i32,
+    }
+
     #[derive(Deserialize)]
     struct Orange {
-        latitude: f32,
+        current_weather: CurrentWeather,
     }
 
     let resp =
-        reqwest::get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41")
+        reqwest::get("https://api.open-meteo.com/v1/forecast?latitude=33.52&longitude=-86.80&current_weather=true&temperature_unit=fahrenheit")
             .await?
             .json::<Orange>()
             .await?;
-    println!("{:#?}", resp.latitude);
+
+    let current_weather = resp.current_weather;
+    println!(
+        "Current Temperature: {:#?}°F",
+        current_weather.temperature as i32
+    );
+    println!(
+        "Current Weather Condition: {}",
+        convert_to_weather_condition(current_weather.weathercode)
+    );
 
     Ok(())
+}
+
+fn convert_to_weather_condition(code: i32) -> String {
+    match code {
+        51 => String::from("Light Drizzle"),
+        _ => String::from("Unknown Weather Code"),
+    }
 }
